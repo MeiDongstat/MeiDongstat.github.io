273707b64c277042f427f364d2968d72a88173d6
MeiDongstat <dongmeistat@gmail.com>
2026-07-23 21:49:34 -0400

    Update website from latest CV

--- _common.R
+++ _common.R
@@ -1,302 +1,117 @@
+library(glue)
 library(htmltools)
-library(stringr)
-library(dplyr)
+library(markdown)
 library(readr)
-library(fontawesome)
 
 knitr::opts_chunk$set(
   collapse = TRUE,
   warning = FALSE,
   message = FALSE,
-  fig.retina = 3,
   comment = "#>"
 )
 
-# gscholar_stats <- function(url) {
-#   cites <- get_cites(url)
-#   return(glue::glue(
-#     'Citations: {cites$citations} | h-index: {cites$hindex} | i10-index: {cites$i10index}'
-#   ))
-# }
-
-# get_cites <- function(url) {
-#   html <- xml2::read_html(url)
-#   node <- rvest::html_nodes(html, xpath='//*[@id="gsc_rsb_st"]')
-#   cites_df <- rvest::html_table(node)[[1]]
-#   cites <- data.frame(t(as.data.frame(cites_df)[,2]))
-#   names(cites) <- c('citations', 'hindex', 'i10index')
-#   return(cites)
-# }
-
-get_pubs <- function() {
-  pubs <- gsheet::gsheet2tbl(
-    url = 'https://docs.google.com/spreadsheets/d/1didInQW61LOz7zroYji-9lGs2v4nYyHnHKMOEFtKVwk/edit?usp=sharing')
-  pubs <- make_citations(pubs)
-  # pubs$summary <- ifelse(is.na(pubs$summary), FALSE, pubs$summary)
-  # pubs$stub <- make_stubs(pubs)
-  # pubs$url_summary <- file.path('research', pubs$stub, "index.html")
-  # pubs$url_scholar <- ifelse(
-  #   is.na(pubs$id_scholar), NA, 
-  #   glue::glue('https://scholar.google.com/citations?view_op=view_citation&hl=en&user=DY2D56IAAAAJ&citation_for_view=DY2D56IAAAAJ:{pubs$id_scholar}')
-  # )
-  return(pubs)
-}
-
-make_citations <- function(pubs) {
-  pubs$citation <- unlist(lapply(split(pubs, 1:nrow(pubs)), make_citation))
-  return(pubs)
+read_publications <- function(path = "content/pubs.csv") {
+  readr::read_csv(
+    path,
+    col_types = readr::cols(.default = readr::col_character()),
+    na = c("", "NA"),
+    show_col_types = FALSE
+  )
 }
 
 make_citation <- function(pub) {
-  if (!is.na(pub$journal)) {
-    pub$journal <- glue::glue('_{pub$journal}_.')
+  title <- if (grepl("[.!?]$", pub$title)) {
+    pub$title
+  } else {
+    paste0(pub$title, ".")
   }
-  if (!is.na(pub$number)) {
-    pub$number <- glue::glue('{pub$number}.')
+
+  year <- if (!is.na(pub$year) && nzchar(pub$year)) {
+    glue::glue(" ({pub$year}).")
+  } else {
+    "."
   }
-  if (!is.na(pub$doi)) {
-    pub$doi <- make_doi(pub$doi)
+
+  venue <- if (!is.na(pub$venue) && nzchar(pub$venue)) {
+    glue::glue(" *{pub$venue}*.")
+  } else {
+    ""
   }
-  pub$year <- glue::glue("({pub$year})")
-  pub$title <- glue::glue('"{pub$title}"')
-  pub[,which(is.na(pub))] <- ''
-  return(paste(
-    pub$author, pub$year, pub$title, pub$journal, 
-    pub$number, pub$doi
-  ))
+
+  details <- if (!is.na(pub$details) && nzchar(pub$details)) {
+    glue::glue(" {pub$details}")
+  } else {
+    ""
+  }
+
+  glue::glue('{pub$authors}{year} "{title}"{venue}{details}')
 }
 
-make_doi <- function(doi) {
-  return(glue::glue('DOI: [{doi}](https://doi.org/{doi})'))
+publication_link <- function(label, url, icon) {
+  if (is.na(url) || !nzchar(url)) {
+    return("")
+  }
+
+  as.character(
+    htmltools::a(
+      href = url,
+      class = "icon-link",
+      target = "_blank",
+      rel = "noopener",
+      HTML(glue::glue('<i class="{icon}"></i> {label}'))
+    )
+  )
 }
 
-# make_stubs <- function(pubs) {
-#   journal <- str_to_lower(pubs$journal)
-#   journal <- str_replace_all(journal, ':', '')
-#   journal <- str_replace_all(journal, '`', '')
-#   journal <- str_replace_all(journal, "'", '')
-#   journal <- str_replace_all(journal, "\\.", '')
-#   journal <- str_replace_all(journal, "&", '')
-#   journal <- str_replace_all(journal, ',', '')
-#   journal <- str_replace_all(journal, '  ', '-')
-#   journal <- str_replace_all(journal, ' ', '-')
-#   return(paste0(pubs$year, '-', journal))
-# }
+make_publication_item <- function(pub) {
+  citation <- markdown::markdownToHTML(
+    text = make_citation(pub),
+    fragment.only = TRUE
+  )
 
-# make_pub_list <- function(pubs, category) {
-#   x <- pubs[which(pubs$category == category),]
-#   pub_list <- list()
-#   for (i in 1:nrow(x)) {
-#     pub_list[[i]] <- make_pub(x[i,], index = i)
-#   }
-#   return(htmltools::HTML(paste(unlist(pub_list), collapse = "")))
-# }
+  doi_url <- if (!is.na(pub$doi) && nzchar(pub$doi)) {
+    paste0("https://doi.org/", pub$doi)
+  } else {
+    NA_character_
+  }
+
+  links <- paste0(
+    publication_link("DOI", doi_url, "fas fa-external-link-alt"),
+    publication_link("Journal", pub$url_pub, "fas fa-book-open"),
+    publication_link("arXiv", pub$url_arxiv, "far fa-file-pdf"),
+    publication_link("Code", pub$url_code, "fab fa-github")
+  )
+
+  glue::glue(
+    '<li><div class="publication-citation">{citation}</div>',
+    '<div class="publication-links">{links}</div></li>'
+  )
+}
 
 make_pub_list <- function(pubs, category) {
-  x <- pubs[ which(pubs$category == category), ]
-  
-  # If there are no pubs in that category, return an empty HTML string
-  if (nrow(x) == 0) {
+  selected <- pubs[pubs$category == category, , drop = FALSE]
+
+  if (nrow(selected) == 0) {
     return(htmltools::HTML(""))
   }
-  
-  # Pre-allocate and loop safely with seq_len()
-  pub_list <- vector("list", nrow(x))
-  for (i in seq_len(nrow(x))) {
-    pub_list[[i]] <- make_pub(x[i, ], index = i)
-  }
-  
-  htmltools::HTML(paste0(unlist(pub_list), collapse = ""))
-}
 
-
-make_pub <- function(pub, index = NULL) {
-  header <- FALSE
-  # altmetric <- make_altmetric(pub)
-  if (is.null(index)) {
-    cite <- pub$citation
-    icons <- make_icons(pub)
-  } else {
-    cite <- glue::glue('{index}) {pub$citation}')
-    icons <- glue::glue('<ul style="list-style: none;"><li>{make_icons(pub)}</li></ul>')
-    if (index == 1) { header <- TRUE }
-  }
-  #### return(markdown_to_html(cite))
-  return(htmltools::HTML(glue::glue(
-    '<div class="pub">
-     <div class="grid">
-       <div class="g-col-12"> {markdown_to_html(cite)} </div>
-     </div>
-     {icons}
-   </div>'
-  )))
-}
-
-# make_altmetric <- function(pub) {
-#   altmetric <- ""
-#   if (isTRUE(pub$category == "peer_reviewed")) {
-#     altmetric <- glue::glue('<div data-badge-type="donut" data-doi="{pub$doi}" data-hide-no-mentions="true" class="altmetric-embed"></div>')
-#   }
-#   return(altmetric)
-# }
-
-# make_haiku <- function(pub, header = FALSE) {
-#   html <- ""
-#   haiku <- em(
-#     pub$haiku1, HTML("&#8226;"), 
-#     pub$haiku2, HTML("&#8226;"), 
-#     pub$haiku3
-#   )
-#   if (!is.na(pub$haiku1)) {
-#     if (header) {
-#       html <- as.character(aside_center(list(
-#         HTML("<b>Haiku Summary</b>"), br(), haiku))
-#       )
-#     } else {
-#       html <- as.character(aside_center(list(haiku)))
-#     }
-#   }
-#   return(html)
-# }
-
-aside <- function(text) {
-  return(tag("aside", list(text)))
-}
-
-center <- function(text) {
-  return(tag("center", list(text)))
-}
-
-aside_center <- function(text) {
-  return(aside(center(list(text))))
-}
-
-aside_center_b <- function(text) {
-  return(aside(center(list(tag("b", text)))))
-}
-
-markdown_to_html <- function(text) {
-  if (is.null(text)) { return(text) }
-  
-  # Replace the author names with underlined last names
-  text <- gsub(
-    pattern = "\\\\\\*([^,]+), ([^,]+)", 
-    replacement = "<u>\\\\*\\1</u>, \\2", 
-    text
+  items <- vapply(
+    seq_len(nrow(selected)),
+    function(i) make_publication_item(selected[i, ]),
+    character(1)
   )
-  text <- gsub(
-    pattern = "\\\\\\*\\\\\\*([^,]+), ([^,]+)", 
-    replacement = "<u>\\\\*\\\\*\\1</u>, \\2", 
-    text
+
+  htmltools::HTML(
+    paste0('<ol class="publication-list">', paste(items, collapse = ""), "</ol>")
   )
-  
-  # Render the text as HTML
-  return(HTML(markdown::renderMarkdown(text = text)))
 }
 
-make_icons <- function(pub) {
-  html <- c()
-  # if (isTRUE(pub$summary)) {
-  #   html <- c(html, as.character(icon_link(
-  #     icon = "fas fa-external-link-alt",
-  #     text = "Summary",
-  #     url  = pub$url_summary, 
-  #     class = "icon-link-summary", 
-  #     target = "_self"
-  #   )))      
-  # }
-  if (isTRUE(!is.na(pub$url_pub) && nzchar(pub$url_pub))) {
-    html <- c(html, as.character(icon_link(
-      icon = "fas fa-external-link-alt",
-      text = "Journal",
-      url  = pub$url_pub
-    )))
-  }
-  if (isTRUE(!is.na(pub$url_arXiv) && nzchar(pub$url_arXiv))) {
-    html <- c(html, as.character(icon_link(
-      icon = "fa fa-file-pdf",
-      text = "arXiv",
-      url  = pub$url_arXiv
-    )))
-  }
-  if (isTRUE(!is.na(pub$url_code) && nzchar(pub$url_code))) {
-    html <- c(html, as.character(icon_link(
-      icon = "fab fa-github",
-      text = "Code",
-      url  = pub$url_code
-    )))
-  }
-  # if (isTRUE(!is.na(pub$url_other) && nzchar(pub$url_other))) {
-  #   html <- c(html, as.character(icon_link(
-  #     icon = "fas fa-external-link-alt",
-  #     text = pub$other_label,
-  #     url  = pub$url_other
-  #   )))
-  # }
-  # if (isTRUE(!is.na(pub$url_rg) && nzchar(pub$url_rg))) {
-  #   html <- c(html, as.character(icon_link(
-  #     icon = "ai ai-researchgate",
-  #     # text = "&nbsp;",
-  #     text = "RG",
-  #     url  = pub$url_rg
-  #   )))
-  # }
-  # if (isTRUE(!is.na(pub$url_scholar) && nzchar(pub$url_scholar))) {
-  #   html <- c(html, as.character(icon_link(
-  #     icon = "ai ai-google-scholar",
-  #     # text = "&nbsp;",
-  #     text = "Scholar",
-  #     url  = pub$url_scholar
-  #   )))
-  # }
-  return(paste(html, collapse = ""))
-}
-
-# The icon_link() function is in {distilltools}, but I've modified this
-# one to include  a custom class to be able to have more control over the
-# CSS and an optional target argument
-
-icon_link <- function(
-    icon = NULL,
-    text = NULL,
-    url = NULL,
+icon_link <- function(text, url) {
+  htmltools::a(
+    href = url,
+    text,
     class = "icon-link",
-    target = "_blank"
-) {
-  if (!is.null(icon)) {
-    text <- make_icon_text(icon, text)
-  }
-  return(htmltools::a(
-    href = url, text, class = class, target = target, rel = "noopener"
-  ))
-}
-
-make_icon_text <- function(icon, text) {
-  return(HTML(paste0(make_icon(icon), " ", text)))
-}
-
-make_icon <- function(icon) {
-  return(tag("i", list(class = icon)))
-}
-
-last_updated <- function() {
-  return(span(
-    paste0(
-      'Last updated on ',
-      format(Sys.Date(), format="%B %d, %Y")
-    ),
-    style = "font-size:0.8rem;")
+    target = "_self",
+    rel = "noopener"
   )
 }
-
-# make_media_list <- function() {
-#   media <- gsheet::gsheet2tbl(
-#     url = 'https://docs.google.com/spreadsheets/d/1xyzgW5h1rVkmtO1rduLsoNRF9vszwfFZPd72zrNmhmU/edit#gid=2088158801')
-#   temp <- media %>% 
-#     mutate(
-#       date = format(date, format = "%b %d, %Y"), 
-#       outlet = paste0("**", outlet, "**"),
-#       post = paste0("- ", date, " - ", outlet, ": ", post)
-#     )
-#   return(paste(temp$post, collapse = "\n"))
-# }
